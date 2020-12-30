9e72f7565b8839fe4aaf809a96a28eb973a47bf8
zulma <zumendez7@gmail.com>
2020-12-30 14:34:02 +0100

    exo fini

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+//  # Exercice 1 JavaScript -> Array
+
+// - ## Créer une variable qui reçois un array avec 3 éléments -> ex: coding 15, coding 16, coding 17
+let elem = ["coding 15", "coding 16", "coding 17"]
+
+// - ##  Afficher les éléments de l'array
+console.log(elem)
+
+// - ## Afficher le nombre d'éléments de l'array
+console.log(elem.length)
+
+// - ## Afficher les éléments de l'array 1 par 1 sans utiliser une boucle
+console.log(elem[0])
+console.log(elem[1])
+console.log(elem[2])
